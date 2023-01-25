fd5f4b37e5397d8526eeadbf8ecbe6d5349d554d
Le Duy Thuc <leduythuc.cs@gmail.com>
2023-01-25 05:32:24 +0000

    Add compile endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @Get()
+    getHello(): string {
+        return this.appService.getHello();
+    }
+
+    @Post('compile')
+    compile(@Body() body: { source: string }) {
+        return this.appService.compile(body.source);
+    }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,59 @@
 import { Injectable } from '@nestjs/common';
+const solc = require('solc');
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+    getHello(): string {
+        return 'Hello World!';
+    }
+
+    compile(source: string) {
+        const contractFile = 'contract.sol';
+        const input = {
+            language: 'Solidity',
+            sources: {
+                'contract.sol': {
+                    content: source,
+                },
+            },
+            settings: {
+                outputSelection: {
+                    '*': {
+                        '*': ['evm.bytecode'],
+                    },
+                },
+            },
+        };
+        const output = JSON.parse(solc.compile(JSON.stringify(input)));
+        const allErrors: {
+            severity: string;
+            formattedMessage: string;
+            errorCode: string;
+        }[] = output['errors'] ?? [];
+
+        const errors = allErrors.filter((x) => x.severity === 'error');
+
+        if (errors.length > 0) {
+            return {
+                bytecode: '',
+                errors: errors.map((x) => {
+                    return {
+                        msg: x.formattedMessage,
+                        code: x.errorCode,
+                    };
+                }),
+            };
+        }
+
+        for (const contractName in output.contracts[contractFile]) {
+            return {
+                bytecode:
+                    '0x' +
+                    output.contracts[contractFile][contractName].evm.bytecode
+                        .object,
+                errors: [],
+            };
+        }
+        return { bytecode: '', msg: 'Compilation failed - no contract found' };
+    }
 }
